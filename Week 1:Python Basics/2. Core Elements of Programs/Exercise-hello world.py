b2508e1e2e1a37cfde279d212ca9e1dbacb757ea
cuichacha <chxdtc123@gmail.com>
2019-02-22 19:03:01 +0800

    Add files via upload

---
+++ Week 1:Python Basics/2. Core Elements of Programs/Exercise-hello world.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 22 09:00:51 2019
+
+@author: Will
+"""
+
+print("hello world")
---
+++ Week 1:Python Basics/2. Core Elements of Programs/Exercise-while-2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 22 09:50:42 2019
+
+@author: Will
+"""
+
+print("Hello")
+num=10
+while num>=2:
+     print(num)
+     num-=2
