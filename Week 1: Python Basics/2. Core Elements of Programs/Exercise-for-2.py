9592af0dfefa17b63fe0e8529bd9d0c124e8ca53
cuichacha <chxdtc123@gmail.com>
2019-02-25 20:50:43 +0800

    Add files via upload

---
+++ Week 1: Python Basics/2. Core Elements of Programs/Exercise-for-2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 22 09:59:24 2019
+
+@author: Will
+"""
+
+print("Hello")
+for i in range (10,0,-2):
+     print(i)
---
+++ Week 1: Python Basics/2. Core Elements of Programs/Exercise-for-3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 22 10:01:53 2019
+
+@author: Will
+"""
+
+num=0
+for i in range (1,(end+1)):
+     num+=i
+print(num)
